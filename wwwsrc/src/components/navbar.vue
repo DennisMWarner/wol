8e798bfb28cee169311532bfb3ec021c9928f3ac
DennisMWarner <warner.dennis.m@gmail.com>
2020-06-17 22:19:47 -0600

    init

---
+++ wwwsrc/src/components/navbar.vue
@@ -0,0 +1,111 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">Cognifit</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >Settings</a>
+          <div class="dropdown-menu text-center" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Action</a>
+            <button class="btn btn-success">Change Workout</button>
+            <button class="btn btn-success">Change Workout</button>
+            <button class="btn btn-success">Change Workout</button>
+            <button class="btn btn-success">Change Workout</button>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" :to="{ name: 'home' }">CogniFit</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item" :class="{ active: $route.name == 'home' }">
+          <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
+        </li>
+        <li
+          class="nav-item"
+          v-if="$auth.isAuthenticated"
+          :class="{ active: $route.name == 'dashboard' }"
+        >
+          <router-link class="nav-link" :to="{ name: 'dashboard' }">My-Dashboard</router-link>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        <button class="btn btn-success" @click="login" v-if="!$auth.isAuthenticated">Login</button>
+        <button class="btn btn-danger" @click="logout" v-else>logout</button>
+      </span>
+    </div>
+  </nav>-->
+</template>
+
+<script>
+import axios from "axios";
+
+let _api = axios.create({
+  baseURL: "https://localhost:5001",
+  withCredentials: true
+});
+export default {
+  name: "Navbar",
+  methods: {
+    async login() {
+      await this.$auth.loginWithPopup();
+      this.$store.dispatch("setBearer", this.$auth.bearer);
+      console.log("this.$auth.user: ");
+      console.log(this.$auth.user);
+    },
+    async logout() {
+      this.$store.dispatch("resetBearer");
+      await this.$auth.logout({ returnTo: window.location.origin });
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ wwwsrc/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home bg-primary"></div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
+</script>
